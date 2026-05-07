d5447d96fe8f7f2970db5bff7c8a915c1b7f5e73
ConnorPetit <cpetit465@gmail.com>
2026-05-07 03:33:34 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
+const slides = document.querySelectorAll('.slide');
+const next = document.querySelector('.next');
+const prev = document.querySelector('.prev');
 
+let current = 0;
+
+function showSlide(index){
+  slides.forEach(slide => slide.classList.remove('active'));
+  slides[index].classList.add('active');
+}
+
+next.addEventListener('click', () => {
+  current = (current + 1) % slides.length;
+  showSlide(current);
+});
+
+prev.addEventListener('click', () => {
+  current = (current - 1 + slides.length) % slides.length;
+  showSlide(current);
+});
+
+setInterval(() => {
+  current = (current + 1) % slides.length;
+  showSlide(current);
+}, 5000);
